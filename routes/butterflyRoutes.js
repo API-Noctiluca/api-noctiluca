c455cf03ace977fdbf9739c307444c5c45cc43b6
Aday25 <aday.alvarez84@gmail.com>
2025-09-01 16:57:27 +0200

    Save current changes before rebasing with develop

--- routes/butterflyRoutes.js
+++ routes/butterflyRoutes.js
@@ -4,7 +4,7 @@ const router = Router();
 
 // Ruta de prueba
 router.get("/", (req, res) => {
-  res.json({ mensaje: "Ruta de mariposas funcionando 🚀" });
+  res.json({ mensaje: "Ruta de mariposas funcionando ✅​👏🏼​" });
 });
 
 export default router;
