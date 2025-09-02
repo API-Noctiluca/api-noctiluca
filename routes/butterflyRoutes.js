555fe999020d4f27e45fa22429d30e97bf435f88
Aday25 <aday.alvarez84@gmail.com>
2025-09-02 18:33:27 +0200

    feat(routes): add validation middleware to butterfly POST and DELETE routes

--- routes/butterflyRoutes.js
+++ routes/butterflyRoutes.js
@@ -1,14 +1,17 @@
 import express from "express";
-import {
-  getAllButterflies,
-  createButterfly,
-  deleteButterfly,
-} from "../controllers/ButterflyController.js";
+import {getAllButterflies, createButterfly,deleteButterfly} from "../controllers/ButterflyController.js";
+
+import {butterflyBodyRules, idParamRules, validateResult} from "../middlewares/butterfliesValidator.js";
 
 const router = express.Router();
 
-router.post("/", createButterfly);
-router.delete("/:id", deleteButterfly);
+// GET - listado de todas las mariposas
 router.get("/", getAllButterflies);
+
+// POST - crear mariposa (con validaciones)
+router.post("/", butterflyBodyRules, validateResult, createButterfly);
+
+// DELETE - borrar mariposa (validar id)
+router.delete("/:id", idParamRules, validateResult, deleteButterfly);
 
 export default router;
