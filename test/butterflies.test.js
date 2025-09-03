d9d8f748bb306745b9c195dfb671396f30fa49b4
guiss26 <guissella584@gmail.com>
2025-09-03 10:52:33 -0700

    test(butterflies): ensure delete method test works (#24)

--- test/butterflies.test.js
+++ test/butterflies.test.js
@@ -36,38 +36,38 @@ describe("test butterflies crud", () => {
     //Método PUT one butterfly/:id
 
     //Método DELETE one butterfly/:id
-    // describe("DELETE /butterflies", () =>{
-    //     let response
-    //     let createdButterfly = {}
+    describe("DELETE /butterflies", () =>{
+        let response
+        let createdButterfly = {}
 
-    //     beforeEach(async () => {//antes de cada test, crea una mariposa y hace la petición
-    //         createdButterfly = await ButterflyModel.create({
-    //             name: "test",
-    //             other_names: "test",
-    //             family: "test",
-    //             location: "test",
-    //             habitat: "test",
-    //             morphology: "test",
-    //             life: "test",
-    //             feeding: "test",
-    //             conservation: "test",
-    //             about_conservation: "test",
-    //             image: "test"
-    //         })
-    //         response = await request(app).delete(`/butterflies/${createdButterfly.id}`).send()
-    //     })
+        beforeEach(async () => {//antes de cada test, crea una mariposa y hace la petición
+            createdButterfly = await ButterflyModel.create({
+                name: "test",
+                other_names: "test",
+                family: "test",
+                location: "test",
+                habitat: "test",
+                morphology: "test",
+                life: "test",
+                feeding: "test",
+                conservation: "test",
+                about_conservation: "LC",
+                image: "test"
+            })
+            response = await request(app).delete(`/api/butterflies/${createdButterfly.id}`).send()
+        })
 
-    //     test('should return a response with status 200 and type json', async () =>{
-    //         expect(response.status).toBe(200)
-    //         expect(response.headers['content-type']).toContain('json')
-    //     })
+        test('should return a response with status 200 and type json', async () =>{
+            expect(response.status).toBe(200)
+            expect(response.headers['content-type']).toContain('json')
+        })
 
-    //     test('should return a message butterfly deleted successfully', async () => {
-    //         expect(response.body.message).toContain("The butterfly has been deleted successfully")
-    //         const foundButterfly = await ButterflyModel.findOne({where: {id: createdButterfly.id}})
-    //         expect(foundButterfly).toBeNull();
-    //     })
-    // })
+        test('should return a message butterfly deleted successfully', async () => {
+            expect(response.body.message).toContain("Butterfly deleted successfully")
+            const foundButterfly = await ButterflyModel.findOne({where: {id: createdButterfly.id}})
+            expect(foundButterfly).toBeNull();
+        })
+    })
 
     afterAll(async() => { // funciona como el beforeAll, después de cada testeo lo apagamos/cerramos
         server.close() // para que Jest no se quede colgado
